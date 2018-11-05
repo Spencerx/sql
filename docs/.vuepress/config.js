5896f2d86540899f45df4903dfdba0a6cd86fbd7
son9912 <son9912@gmail.com>
2018-11-05 11:13:11 +0700

    Add license + update on package Also do a minor change in routing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     base: '/core/',
-    title: 'Kuma Cheatsheet Core',
-    description: 'Core of all cheatsheet',
+    title: 'Kuma Cheatsheet',
+    description: 'Cheatsheet made easy',
 
     // https://vuepress.vuejs.org/config/#head
     head: [
-        ['link', { rel: 'icon', href: '/logo.png' }]
+        ['link', { rel: 'icon', href: 'logo.png' }]
     ],
 
     // https://vuepress.vuejs.org/config/#serviceworker
@@ -14,5 +14,22 @@ module.exports = {
     // https://vuepress.vuejs.org/config/#markdown
     markdown: {
         lineNumbers: 'True'
+    },
+
+    // https://vuepress.vuejs.org/plugin/official/plugin-pwa.html#install
+    plugins: [
+        ['@vuepress/pwa'],
+        ['@vuepress/medium-zoom']
+    ],
+
+    // https://vuepress.vuejs.org/plugin/official/plugin-pwa.html#options
+    updatePopup: 'True',
+
+    // https://vuepress.vuejs.org/theme/default-theme-config.html#sidebar
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Contribute', link: 'https://github.com/Kuma-Cheatsheet/' },
+        ],
     }
 }
