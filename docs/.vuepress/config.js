a37c6ed2ac8814330ec5d625dac4bfd0e73a88a2
son9912 <son9912@gmail.com>
2019-01-28 19:54:38 +0700

    Add multiple language support

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,7 @@
 module.exports = {
+    // Global Variables + Edits
     // Make sure that base + repo are in correct cheatsheet
     base: '/core/',
-    title: 'Kuma Cheatsheet',
-    description: 'Cheatsheet made easy',
 
     repo: 'Kuma-Cheatsheet/core',
     repoLabel: 'Repository',
@@ -34,79 +33,88 @@ module.exports = {
         ['@vuepress/blog'],
     ],
 
-    // Official : https://vuepress.vuejs.org/plugin/official/plugin-pwa.html#options
+    // When there is website version, notify user
+    // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#service-worker
     updatePopup: 'True',
 
-    // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#sidebar
+    // Editing themes
+    // Official : https://vuepress.vuejs.org/theme/default-theme-config.html
     themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            {
-                text: 'Contribution',
-                items: [
-                    {
-                        text: 'Contribute', items: [
-                            { text: 'Report a problem', link: 'https://github.com/Kuma-Cheatsheet/core/issues/' },
-                            { text: 'Request new resource', link: 'https://github.com/Kuma-Cheatsheet/core/issues/' },
-                            { text: 'Contribute new resource', link: 'https://github.com/Kuma-Cheatsheet/core/' },
-                            { text: 'Development Board', link: 'https://github.com/Kuma-Cheatsheet/core/projects' },
-                        ]
-                    },
-                    {
-                        text: 'Guidelines', items: [
-                            { text: 'Contribution Guidelines', link: 'https://github.com/Kuma-Cheatsheet/core/blob/master/CONTRIBUTING.md' },
-                            { text: 'Licensing', link: 'https://github.com/Kuma-Cheatsheet/core/https://github.com/Kuma-Cheatsheet/core/blob/master/LICENSE.md/' },
-                        ]
-                    },
-                ]
-            },
-            {
-                text: 'Other Cheatsheet',
-                items: [
-                    {
-                        text: 'Programming Languages', items: [
-                            { text: 'Python', link: 'http://kuma-cheatsheet.github.io/python' },
-                            { text: 'SQL', link: 'http://kuma-cheatsheet.github.io/sql' },
-                        ]
-                    },
-                    {
-                        text: 'Tools', items: [
-                            { text: 'SPSS', link: 'http://kuma-cheatsheet.github.io/spss' },
-                            { text: 'Git', link: 'http://kuma-cheatsheet.github.io/git' },
-                        ]
-                    },
-                    {
-                        text: 'Diagrams', items: [
-                            { text: 'Class Diagram (UML)', link: 'http://kuma-cheatsheet.github.io/class-diagram' },
-                        ]
-                    },
-                    {
-                        text: 'Others', items: [
-                            { text: 'See all', link: 'https://github.com/Kuma-Cheatsheet' },
-                        ]
-                    }
-                ]
-            },
-            {
-                // All child cheatsheet can ignore this if there is no good documentation
-                text: 'Documentation',
-                items: [
-                    { text: 'Official', link: '' },
-                    { text: 'DevDocs', link: 'https://devdocs.io/markdown' },
-                ]
-            },
-        ],
+        // Supports of mulitple languages
+        // https://vuepress.vuejs.org/guide/i18n.html#default-theme-i18n-config
+        locales: {
+            '/': {
+                lang: 'en-US',
 
-        sidebar: [
-            // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#sidebar
-        ],
+                title: 'Kuma Cheatsheet',
+                description: 'Cheatsheet made easy',
 
-        // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#last-updated
-        lastUpdated: 'อับเดทล่าสุด',
+                nav: [
+                    { text: 'Home', link: '/' },
+                    {
+                        text: 'Contribution',
+                        items: [
+                            {
+                                text: 'Contribute', items: [
+                                    { text: 'Report a problem', link: 'https://github.com/Kuma-Cheatsheet/core/issues/' },
+                                    { text: 'Request new resource', link: 'https://github.com/Kuma-Cheatsheet/core/issues/' },
+                                    { text: 'Contribute new resource', link: 'https://github.com/Kuma-Cheatsheet/core/' },
+                                    { text: 'Development Board', link: 'https://github.com/Kuma-Cheatsheet/core/projects' },
+                                ]
+                            },
+                            {
+                                text: 'Guidelines', items: [
+                                    { text: 'Contribution Guidelines', link: 'https://github.com/Kuma-Cheatsheet/core/blob/master/docs/CONTRIBUTING.md' },
+                                    { text: 'Licensing', link: 'https://github.com/Kuma-Cheatsheet/core/https://github.com/Kuma-Cheatsheet/core/blob/master/LICENSE.md/' },
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        text: 'Other Cheatsheet',
+                        items: [
+                            {
+                                text: 'Programming Languages', items: [
+                                    { text: 'Python', link: 'http://kuma-cheatsheet.github.io/python' },
+                                    { text: 'SQL', link: 'http://kuma-cheatsheet.github.io/sql' },
+                                ]
+                            },
+                            {
+                                text: 'Tools', items: [
+                                    { text: 'SPSS', link: 'http://kuma-cheatsheet.github.io/spss' },
+                                    { text: 'Git', link: 'http://kuma-cheatsheet.github.io/git' },
+                                ]
+                            },
+                            {
+                                text: 'Diagrams', items: [
+                                    { text: 'Class Diagram (UML)', link: 'http://kuma-cheatsheet.github.io/class-diagram' },
+                                ]
+                            },
+                            {
+                                text: 'Others', items: [
+                                    { text: 'See all', link: 'https://github.com/Kuma-Cheatsheet' },
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        // All child cheatsheet can ignore this if there is no good documentation
+                        text: 'Documentation',
+                        items: [
+                            { text: 'Official', link: '' },
+                            { text: 'DevDocs', link: '' },
+                        ]
+                    },
+                ],
 
-        // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#service-worker
-        serviceWorker: {
-            updatePopup: true,
+                sidebar: [
+                    // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#sidebar
+
+                ],
+
+                // Official : https://vuepress.vuejs.org/theme/default-theme-config.html#last-updated
+                lastUpdated: 'อับเดทล่าสุด',
+            }
         },
-    }
+    },
 }
